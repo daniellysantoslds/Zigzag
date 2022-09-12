f98d2aac2b31eefde9700e9e286b0ae791ebe82c
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-09-12 08:56:40 -0300

    adicionando preview do swiftui no viewcode

--- Zigzag/ViewController.swift
+++ Zigzag/ViewController.swift
@@ -8,14 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .blue
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
+// MARK: - Preview
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13, *)
+struct View_Preview: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ViewController().showPreview().previewDevice("iPhone 13")
+            ViewController().showPreview().previewDevice("iPhone 13").previewInterfaceOrientation(.landscapeLeft)
+        }
+    }
+}
+#endif
