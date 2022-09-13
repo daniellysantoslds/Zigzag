3b010cff2cd65d2c66cae083f344ff6aa39f91e8
msoliveiranatalia <nmso@academy.cin.ufpe.br>
2022-09-13 11:58:44 -0300

    base da collection view

--- Zigzag/Controllers/BuscarViewController.swift
+++ Zigzag/Controllers/BuscarViewController.swift
@@ -7,10 +7,32 @@
 
 import UIKit
 
-class BuscarViewController: UIViewController {
+// acrescentei UICollectionViewDelegate e UICollectionViewDataSource
+class BuscarViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    //collection view
+    private var collectionView: UICollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //collection view layout
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        //desembrulhando (unwrap) a collection view pq o valor tava opcional
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
 
         view.backgroundColor = .systemBackground
     }
@@ -25,5 +47,17 @@ class BuscarViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.contentView.backgroundColor = .systemBlue
+        return cell
+    }
+    
+    
+    
 }
