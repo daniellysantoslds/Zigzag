7247877d5f0305e3f14ae1e7c7820a9542824a7d
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-09-13 20:49:12 -0300

    add configurações da tableview + tableviewcell

--- Zigzag/Controllers/ExplorarViewController.swift
+++ Zigzag/Controllers/ExplorarViewController.swift
@@ -8,21 +8,53 @@
 import UIKit
 
 class ExplorarViewController: UIViewController {
-
+    
+    
+    private let explorarFeedTable: UITableView  =  {
+        
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+        
+    }()
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        //adicionando visualizacao da table
+        view.addSubview(explorarFeedTable)
+        
+        explorarFeedTable.delegate = self
+        explorarFeedTable.dataSource = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        explorarFeedTable.frame = view.bounds
     }
-    */
+    
+    
+    
+}
 
+extension  ExplorarViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "zigzag"
+        return cell
+    }
+    
+    
+    
 }
