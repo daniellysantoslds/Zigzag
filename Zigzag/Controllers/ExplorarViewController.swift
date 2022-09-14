9eb9d8df62f527047292c4e65caaba7e05f8ca0d
Danielly Santos <102704880+daniellysantoslds@users.noreply.github.com>
2022-09-13 21:34:30 -0300

    Revert "Tela explorar"

--- Zigzag/Controllers/ExplorarViewController.swift
+++ Zigzag/Controllers/ExplorarViewController.swift
@@ -8,90 +8,21 @@
 import UIKit
 
 class ExplorarViewController: UIViewController {
-    
-    
-    private let explorarFeedTable: UITableView  =  {
-        
-        let table = UITableView(frame: .zero, style: .grouped)
-        table.register(ExplorarCollectionViewTableViewCell.self, forCellReuseIdentifier: ExplorarCollectionViewTableViewCell.identifier)
-        return table
-        
-    }()
-    
-    
-    
-    
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        //adicionando visualizacao da table
-        view.addSubview(explorarFeedTable)
-        
-        explorarFeedTable.delegate = self
-        explorarFeedTable.dataSource = self
-        
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        explorarFeedTable.frame = view.bounds
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    
-    
+    */
+
 }
-
-extension  ExplorarViewController: UITableViewDelegate, UITableViewDataSource {
-    
-   
-    //numero de sessoes
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 4
-    }
-    
-    //numero de celulas
-    func tableView(_ tableview: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: ExplorarCollectionViewTableViewCell.identifier, for: indexPath) as? ExplorarCollectionViewTableViewCell else {
-            return UITableViewCell()
-        }
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 40
-    }
-    
-}
-
-
-
-// MARK: - Preview
-#if DEBUG
-import SwiftUI
-
-@available(iOS 13, *)
-struct ExplorarViewController_Preview: PreviewProvider {
-    static var previews: some View {
-        Group {
-            ExplorarViewController().showPreview().previewDevice("iPhone 13")
-            ExplorarViewController().showPreview().previewDevice("iPhone 13").previewInterfaceOrientation(.landscapeLeft)
-        }
-    }
-}
-#endif
