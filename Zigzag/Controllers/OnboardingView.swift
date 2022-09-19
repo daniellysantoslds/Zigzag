e33c396709a9a530fc98c8485aa344aacd7cd72f
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-09-19 09:44:03 -0300

    subindo consfiguracoes

---
+++ Zigzag/Controllers/OnboardingView.swift
@@ -0,0 +1,157 @@
+//
+//  OnboardingView.swift
+//  Zigzag
+//
+//  Created by Danielly Santos Lopes da Silva on 19/09/22.
+//
+import UIKit
+
+class OnboardingView: UIView {
+
+   
+
+        // MARK: - inicializar as views
+        let backgroundView = UIImageView()
+        let backgroundLinearView = UIImageView()
+        let tituloLabel = UILabel()
+        let descriçãoLabel = UILabel()
+        let comecarButton = UIButton(configuration: .filled())
+
+
+        let tituloLabelContainer = UIView()
+        let descriçãoLabelContainer = UIView()
+        let comecarButtonContainer = UIView()
+
+        let stackView = UIStackView()
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViewsHierarqchy()
+        setupViewsAttributes()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+        // MARK: - Setup na hierarquia das views
+    private func setupViewsHierarqchy() {
+        addSubview(backgroundView)
+        addSubview(backgroundLinearView)
+        addSubview(stackView)
+        
+        tituloLabelContainer.addSubview(tituloLabel)
+        descriçãoLabelContainer.addSubview(descriçãoLabel)
+        comecarButtonContainer.addSubview(comecarButton)
+
+        stackView.addArrangedSubview(tituloLabelContainer)
+        stackView.addArrangedSubview(descriçãoLabelContainer)
+        stackView.addArrangedSubview(comecarButtonContainer)
+        
+    }
+       
+    private func setupViewsAttributes() {
+        
+        // MARK: - Conf dos atributos dessas vies
+        backgroundView.image = UIImage(named: "bg-onboarding")
+        backgroundView.contentMode = .scaleAspectFill
+
+        tituloLabel.text = "oii"
+
+        descriçãoLabel.text = "oii"
+
+
+        comecarButton.setTitle("Começar", for: .normal)
+        comecarButton.setTitleColor(.label, for: .normal)
+        comecarButton.tintColor = .systemOrange
+
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 32
+
+    }
+
+
+    func setupConstraints() {
+        // MARK: - Conf das constrainsts e autolayout
+        
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: topAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+
+        tituloLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tituloLabel.topAnchor.constraint(equalTo: tituloLabelContainer.topAnchor, constant: 44),
+            tituloLabel.trailingAnchor.constraint(equalTo: tituloLabelContainer.trailingAnchor),
+            tituloLabel.leadingAnchor.constraint(equalTo: tituloLabelContainer.leadingAnchor),
+            tituloLabel.bottomAnchor.constraint(equalTo: tituloLabelContainer.bottomAnchor)
+        ])
+
+        descriçãoLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            descriçãoLabel.topAnchor.constraint(equalTo: descriçãoLabelContainer.topAnchor),
+            descriçãoLabel.trailingAnchor.constraint(equalTo: descriçãoLabelContainer.trailingAnchor),
+            descriçãoLabel.leadingAnchor.constraint(equalTo: descriçãoLabelContainer.leadingAnchor),
+            descriçãoLabel.bottomAnchor.constraint(equalTo: descriçãoLabelContainer.bottomAnchor)
+        ])
+        
+        comecarButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            comecarButton.centerXAnchor.constraint(equalTo: comecarButtonContainer.centerXAnchor),
+            comecarButton.centerYAnchor.constraint(equalTo: comecarButtonContainer.centerYAnchor),
+            comecarButton.trailingAnchor.constraint(equalTo: comecarButton.trailingAnchor, constant: -40),
+            comecarButton.leadingAnchor.constraint(equalTo: comecarButtonContainer.leadingAnchor, constant: 40),
+            comecarButton.heightAnchor.constraint(equalToConstant: 44)
+        
+        
+        ])
+
+    }
+       
+       
+
+
+func setupAdditionalConfiguration() {
+    // MARK: - Outras configurações
+  comecarButton.addTarget(self, action: #selector(tappedButton), for: .touchUpInside)
+}
+
+@objc func tappedButton(sender: UIButton) {
+    print("tocou botão")
+   
+}
+
+}
+
+// MARK: - Preview
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13, *)
+struct OnboardingView_Preview: PreviewProvider {
+    static var previews: some View {
+        Group {
+           OnboardingView().showPreview().previewDevice("iPhone 13")
+           OnboardingView().showPreview().previewDevice("iPhone 13").previewInterfaceOrientation(.landscapeLeft)
+
+        }
+    }
+}
+#endif
