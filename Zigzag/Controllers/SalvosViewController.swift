4ebc8fc3213c9a104f59b28122139a2b2608c7ec
Jellybellis <ibc2@academy.cin.ufpe.br>
2022-09-22 21:57:10 -0300

    Adicionando tela de salvos

--- Zigzag/Controllers/SalvosViewController.swift
+++ Zigzag/Controllers/SalvosViewController.swift
@@ -7,24 +7,81 @@
 
 import UIKit
 
-class SalvosViewController: UIViewController {
+    class SalvosViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+        
+        //collection view
+        private var SalvosCollectionView: UICollectionView?
+        
+        public let imagensResults = [
+            UIImage(named: "Espaco-image" ),
+            UIImage(named: "Grand-image" ),
+            UIImage(named: "Veneza-image" ),
+        ]
+        
+        public let textoResults = ["Espaço ciência", "Grand Hotel Recife", "Veneza Water Park"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+         
 
-        view.backgroundColor = .systemBackground
+            
+            
+            view.backgroundColor = .systemCyan
+            
+            //collection view layout
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .vertical
+            layout.itemSize = CGSize(width: view.frame.size.width-view.frame.size.width/20, height: view.frame.size.width/1.5)
+            layout.minimumLineSpacing = 16
+            layout.minimumInteritemSpacing = 16
+            
+            SalvosCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            
+            
+            //desembrulhando (unwrap) a collection view pq o valor tava opcional
+            guard let collectionView = SalvosCollectionView else {
+                return
+            }
+            
+            
+            collectionView.register(ResultadoCollectionViewCell.self, forCellWithReuseIdentifier: ResultadoCollectionViewCell.identifier)
+            collectionView.dataSource = self
+            collectionView.delegate = self
+            view.addSubview(collectionView)
+            collectionView.frame = view.bounds
+
+            view.backgroundColor = .systemBackground
+        }
+        
+        
+        func openDetails(index: Int) {
+            let controller = DetailsViewController()
+            controller.modalPresentationStyle = .fullScreen
+            self.present(controller, animated: true)
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return 2
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ResultadoCollectionViewCell.identifier, for: indexPath)
+            as! ResultadoCollectionViewCell
+            let image = imagensResults[indexPath.item] ?? UIImage()
+            cell.setup(image: image, texto: textoResults[indexPath.item])
+            return cell
+       
+            
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            let cell = collectionView.cellForItem(at: indexPath)
+            openDetails(index: indexPath.row)
+            
+           
+        }
         
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
-}
